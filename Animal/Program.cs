c43bc2844f3e7d90a056b128626374f28310e57c
Emmanuel <ekkwarteng005@st.ug.edu.gh>
2024-06-17 17:02:51 +0000

    An application that demonstrates how Inheritance and Method Overriding is done in C#

---
+++ Animal/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace InheritanceDemo
+{
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Would you like to hear a cat or dog sound?");
+            string userChoice = Console.ReadLine().ToLower();
+
+            if (userChoice == "cat")
+            {
+                Animal myCat = new Cat();
+                myCat.MakeSound();
+            }
+            else if (userChoice == "dog")
+            {
+                Animal myDog = new Dog();
+                myDog.MakeSound();
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Please enter 'cat' or 'dog'.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
