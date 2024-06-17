0d4a242da9f0444fb4369e6a69be4a6270dafe15
Emmanuel <ekkwarteng005@st.ug.edu.gh>
2024-06-17 17:11:39 +0000

    An application that demonstrates how object-oriented programming in C# is done with Abstract Classes and Methods

---
+++ Shapes/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace AbstractClassDemo
+{
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape myCircle = new Circle(5);
+            Shape myRectangle = new Rectangle(4, 6);
+
+            Console.WriteLine($"Area of Circle: {myCircle.GetArea()}");
+            Console.WriteLine($"Area of Rectangle: {myRectangle.GetArea()}");
+
+            Console.ReadLine();
+        }
+    }
+}
